ea9f785a3da0405276c4b9f88344fa806b4863e4
KyeongtaeSon <kyote0428@gmail.com>
2022-05-05 17:12:57 -0500

    final

--- CP_Sp22_FInal/sketch.js
+++ CP_Sp22_FInal/sketch.js
@@ -4,31 +4,36 @@
 // Video
 let video;
 
-let label = 'waiting...';
+let label;
 let classifier;
 let confidence;
 
 // STEP 1: Load the model!
 
 function preload() {
-  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/k1FddoiWG/');
+  classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/XsknySoBQ/');
 }
 
 function setup() {
-  // createCanvas(640, 520);
-  createCanvas(500,300);
-  // Create the video
+  
+  createCanvas(innerWidth,innerHeight);
+  
   video = createCapture(VIDEO);
   video.hide();
+  textAlign(CENTER)
 
   // STEP 2: Start classifying
   classifyVideo();
 }
 
+function windowResized() {
+  resizeCanvas(innerWidth, innerHeight);
+}
+
 function classifyVideo(){
   classifier.classify(video, gotResults);
 }
-// STEP 2 classify!
+
 
 function draw() {
   background(0);
@@ -38,28 +43,26 @@ function draw() {
 
   // STEP 4: Draw the label
   
-  // textSize(32);
-  textAlign(CENTER, CENTER);
-  // fill(255);
-  // text(label, width/2, height -16);
-  
-  let emoji = '👤';
-  if (label == 'Class 1') {
-    emoji = '⭕';
-  } else if (label == 'Class 2') {
-    emoji = '❌';
-  } else if (label == 'waiting...'){
-       emoji = '👋';
+  if (label == 'saic' && confidence > 0.95) {
+    // show neutral screen
+    saicScreen();
+  } else if (label == 'nonsaic') {
+    // show SAIC screen
+    nonSaicScreen();
+  } else {
+    // show non-SAIC screen
+    
+    neutralScreen();
 }
   
-  textSize(256);
-  text(emoji, width/2,height/2);
+  // textSize(256);
+  // text(emoji, width/2,height/2);
 
-  textAlign(LEFT);
-  fill(255, 0, 255)
-  textSize(20);
-  text(confidence, width/8, height/4);
-  text(label, width/8, height/4 + 30);
+  // textAlign(LEFT);
+  // fill(255, 0, 255)
+  // textSize(20);
+  // text(confidence, width/8, height/4);
+  // text(label, width/8, height/4 + 30);
 }
 
 
@@ -74,4 +77,35 @@ function gotResults(error, results){
   
   classifyVideo();
 }
+
+function neutralScreen() {
+  background(90);
+
+  textSize(100);
+  text("SAIC FAMILY MEMBERS ONLY, \n PLEASE SCAN YOUR ID", width/2, height/2)
+
+  // what do we see when the label is neutral
+
+}
+
+function saicScreen() {
+  background(20, 170, 20);
+  // what do we see when the label is saic
+
+  textSize(100);
+  text("SAIC FAMILY MEMBER CONFIRMED! \nENJOY THE SHOW", width/2, height/2)
+
+}
+
+
+function nonSaicScreen() {
+  background(200, 30, 0);
+
+  // what do we see when the label is nonSaic
+
+  textSize(100);
+  text("NON-SAIC MEMBER IDENTIFIED, \nPLEASE ESCORT YOURSELF OUT", width/2, height/2)
+
+}
+
 
